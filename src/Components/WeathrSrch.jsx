16f227a613711666e14a05c21494bae00e6dfdcd
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-16 19:19:20 +0530

    weather app updated

--- src/Components/WeathrSrch.jsx
+++ src/Components/WeathrSrch.jsx
@@ -98,81 +98,72 @@ function WeathrSrch() {
         <div className="row justify-center items-center">
           <div className="col-md-4 mt-16">
             <div className="card bg-dark text-white text-center border-0 rounded">
-              {loading ? (
-                <div>Loading...</div>
-              ) : (
-                <>
-                  <img
-                    src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
-                    className="cardImg rounded"
-                    alt="Weather condition"
-                    onError={(e) =>
-                      (e.target.src =
-                        "https://source.unsplash.com/600x900/?nature")
-                    }
-                  />
-                  <div className="card-img-overlay">
-                    <form onSubmit={handleSubmit}>
-                      <div className="input-group w-75 h-12 mt-4 mx-auto">
-                        <input
-                          type="search"
-                          className="form-control"
-                          placeholder="Search City"
-                          aria-label="Search City"
-                          aria-describedby="basic-addon2"
-                          name="search"
-                          value={input}
-                          onChange={(e) => setInput(e.target.value)}
-                          required
-                        />
-                        <button
-                          type="submit"
-                          className="search input-group-text bg-gray-400"
-                          id="basic-addon2"
-                        >
-                          <b>
-                            <FaSistrix className="text-3xl" />
-                          </b>
-                        </button>
-                      </div>
-                    </form>
-                    <br />
-                    <div className="mt-5 bg-dark bg-opacity-50 py-3 rounded ml-2 mr-2">
-                      <h1 className="card-title text-4xl mt-2 font-bold">
-                        {data.name}
-                      </h1>
-                      <p className="card-text text-xl">
-                        {formatDate()}
-                        <br />
-                        {formatTime()}
-                      </p>
-                      <br />
-                      <hr />
-                      {emojiComponent && (
-                        <div className="text-9xl mt-3 ml-40">
-                          {emojiComponent}
-                        </div>
-                      )}
-                      {data.main && (
-                        <h1 className="mb-5 text-5xl font-bold">
-                          {Math.round(data.main.temp - 273.15)}&deg;C
-                        </h1>
-                      )}
-                      {data.weather[0] && (
-                        <p className="lead fw-border mb-0 font-bold">
-                          {data.weather[0].main}
-                        </p>
-                      )}
-                      {data.main && (
-                        <p className="lead mb-4">
-                          {Math.round(data.main.temp_max - 273.15)}&deg;C |{" "}
-                          {Math.round(data.main.temp_min - 273.15)}&deg;C
-                        </p>
-                      )}
-                    </div>
+              <img
+                src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
+                className="cardImg rounded"
+                alt="Weather condition"
+                onError={(e) =>
+                  (e.target.src = "https://source.unsplash.com/600x900/?nature")
+                }
+              />
+              <div className="card">
+                <form onSubmit={handleSubmit}>
+                  <div className="input-group w-75 h-12 mt-4 mx-auto">
+                    <input
+                      type="search"
+                      className="form-control"
+                      placeholder="Search City"
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="search input-group-text bg-gray-400"
+                      id="basic-addon2"
+                    >
+                      <b>
+                        <FaSistrix className="text-3xl" />
+                      </b>
+                    </button>
                   </div>
-                </>
-              )}
+                </form>
+                <br />
+                <div className="mt-5 bg-dark bg-opacity-50 py-3 rounded ml-2 mr-2">
+                  <h1 className="card-title text-4xl mt-2 font-bold">
+                    {data.name}
+                  </h1>
+                  <p className="card-text text-xl">
+                    {formatDate()}
+                    <br />
+                    {formatTime()}
+                  </p>
+                  <br />
+                  <hr />
+                  {emojiComponent && (
+                    <div className="text-9xl mt-3 ml-40">{emojiComponent}</div>
+                  )}
+                  {data.main && (
+                    <h1 className="mb-5 text-5xl font-bold">
+                      {Math.round(data.main.temp - 273.15)}&deg;C
+                    </h1>
+                  )}
+                  {data.weather[0] && (
+                    <p className="lead fw-border mb-0 font-bold">
+                      {data.weather[0].main}
+                    </p>
+                  )}
+                  {data.main && (
+                    <p className="lead mb-4">
+                      {Math.round(data.main.temp_max - 273.15)}&deg;C |{" "}
+                      {Math.round(data.main.temp_min - 273.15)}&deg;C
+                    </p>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
